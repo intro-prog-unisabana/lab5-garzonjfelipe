cfa8adb97ab75180a5e89f5ea77a0088a46415c6
Juan Felipe Garzon Baron <garzonbaronj@gmail.com>
2026-03-10 16:04:44 -0500

    lab5.1.5

--- modules.py
+++ modules.py
@@ -1,12 +1,13 @@
 import os
 import math
 
-print("Current working directory:", os.getcwd())
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
 
 num = int(input("Enter an integer: "))
 
-log_val = math.log(num)
-print("Log base 2 of", num, "is:", log_val)
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
 
-print("Floor:", math.floor(log_val))
-print("Ceiling:", math.ceil(log_val))
+print(f"Floor: {math.floor(log_val)}")
+print(f"Ceiling: {math.ceil(log_val)}")
